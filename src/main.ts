2defb7b15aa18bfff7e1e7f1f1ea8fe9b5e6e37a
suhaib <muhammedsuhaibpottayil@gmail.com>
2024-09-27 14:01:54 +0530

    feat: add new cors

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,10 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
 
+  const corsOrigins = configService.get<string>('CORS_ORIGIN');
+
   app.enableCors({
-    origin: ['http://localhost:5173'], 
+    origin: corsOrigins.split(','), 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: 'Content-Type, Authorization',
